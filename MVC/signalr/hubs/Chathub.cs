69012d86bc303152530fd3caaa412ab06345891c
Pratik Patil <pratik@sdcdpu.local>
2021-03-24 12:55:26 +0530

    s

---
+++ MVC/signalr/hubs/Chathub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MVC.signalr.hubs
+{
+    public class MyHubC1 : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewmessageToPage(name, message);
+        
+        }
+    }
+}
